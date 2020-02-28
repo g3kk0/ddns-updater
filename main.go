149a76c29cde7b5d902f127cc03dd58195962679
Mark Newman <mark.newman@aldgate-ventures.com>
2020-02-28 23:40:10 +0000

    Add getting external IP

--- main.go
+++ main.go
@@ -2,29 +2,96 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/davecgh/go-spew/spew"
 	"google.golang.org/api/dns/v1"
 )
 
 func main() {
+	project := "crack-braid-160020"
+	record := "foo.mkzd.host"
+	zone := "mkzd-host"
 
-	ctx := context.Background()
-	dnsService, err := dns.NewService(ctx)
+	dnsService, err := newDnsService(project)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//resp, err := dnsService.ManagedZones.Get("crack-braid-160020", "mkzd-host").Context(ctx).Do()
+	dnsIp, err := dnsService.getRecordValue(record, zone)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Get record
-	//resp, err := dnsService.ResourceRecordSets.List("crack-braid-160020", "mkzd-host").Context(ctx).Do()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	currentIp, err := getIpAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Update record
+	if dnsIp != currentIp {
+		fmt.Println("updating DNS record")
+	} else {
+		fmt.Println("DNS record is in sync")
+	}
+
+	//fmt.Printf("ip = %+v\n", ip)
+
+	// fetch ip
+	// check
+
+	//dnsService.updateRecord("foo", "bar")
+
+	// fmt.Printf("ip = %+v\n", ip)
+
+}
+
+type dnsService struct {
+	ctx     context.Context
+	client  *dns.Service
+	project string
+}
+
+func newDnsService(project string) (*dnsService, error) {
+	ctx := context.Background()
+	client, err := dns.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	dnsService := &dnsService{
+		ctx:     ctx,
+		client:  client,
+		project: project,
+	}
+
+	return dnsService, nil
+}
+
+func (s *dnsService) getRecordValue(record, zone string) (string, error) {
+	resp, err := s.client.ResourceRecordSets.List(s.project, zone).Context(s.ctx).Do()
+	if err != nil {
+		return "", err
+	}
+
+	var ip string
+	for _, v := range resp.Rrsets {
+		if v.Name == record+"." {
+			ip = v.Rrdatas[0]
+		}
+	}
+
+	if ip == "" {
+		return "", errors.New("unable to find value in record")
+	}
+
+	return ip, nil
+}
+
+func (s *dnsService) updateRecord(name, ip string) error {
 	change := dns.Change{
 		Additions: []*dns.ResourceRecordSet{
 			&dns.ResourceRecordSet{
@@ -39,10 +106,30 @@ func main() {
 		},
 	}
 
-	resp, err := dnsService.Changes.Create("crack-braid-160020", "mkzd-host", &change).Context(ctx).Do()
+	resp, err := s.client.Changes.Create("crack-braid-160020", "mkzd-host", &change).Context(s.ctx).Do()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	spew.Dump(resp)
+
+	return nil
+}
+
+func getIpAddress() (string, error) {
+	resp, err := http.Get("https://myexternalip.com/raw")
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return string(body), nil
 }
